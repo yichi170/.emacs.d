9f9166446bc7769544f4e7b1d81468670eec0874
yichi170 <yichi170@gmail.com>
2025-07-11 16:10:09 -0500

    Update and rename the config file

--- init.el
+++ init.el
@@ -1,11 +1,11 @@
 (require 'org)
-(org-babel-load-file "~/.emacs.d/frolic.org")
+(org-babel-load-file "~/.emacs.d/config/spry.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(doom-themes doom-theme company consult vertico)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
